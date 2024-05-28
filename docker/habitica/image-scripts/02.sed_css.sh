811cb65176ff5236eb1481144b19be3130fd937e
diyoyo <diyoyo@users.noreply.github.com>
2024-05-29 01:26:25 +0200

    Adding CSS file to habitica's .gitignore

--- docker/habitica/image-scripts/02.sed_css.sh
+++ docker/habitica/image-scripts/02.sed_css.sh
@@ -5,9 +5,10 @@ source .env
 date=$(echo `date +"%y%m%d_%H%M"`)
 echo $date
 
-css_dir="${HABITICA_PARENT_DIR}/habitica/website/client/src/assets/css"
+CSS_BASE_DIR="website/client/src/assets/css"
+css_dir="${HABITICA_PARENT_DIR}/habitica/${CSS_BASE_DIR}"
 
-files="$css_dir/sprites.css $css_dir/sprites/spritesmith-main.css"
+files="sprites.css sprites/spritesmith-main.css"
 
 AWS_IMG_BASE_URL="https://habitica-assets.s3.amazonaws.com/mobileApp/images"
 AIBU=${AWS_IMG_BASE_URL//'/'/'\/'}
@@ -21,6 +22,11 @@ for file in $files;
   do
 	echo "Editing $file"
 
-	sed -i.bak$date s/${AIBU}/${SHIBU}/ $file
+	sed -i.bak$date s/${AIBU}/${SHIBU}/ $css_dir/$file
+	echo ${CSS_BASE_DIR}/$file >> ${HABITICA_PARENT_DIR}/habitica/.gitignore
+
   done;
+
+echo "*.bak*" >> ${HABITICA_PARENT_DIR}/habitica/.gitignore
+
 
